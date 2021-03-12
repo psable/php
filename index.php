928211c9189a2da98a74093d40df4f2cc7d075ac
psable <52033359+psable@users.noreply.github.com>
2021-03-12 12:48:33 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+echo "<h1>Hello World from PHP7-FPM</h1>";
+echo 'Version PHP courante : ' . phpversion();
+
+echo '<pre>';
+try{
+  $conn = new \PDO('mysql:host=mariadb-service', 'root', 'roottoor');
+  $sth = $conn->prepare('SHOW DATABASES');
+  $sth->execute();
+  $checks = $sth->fetchAll(PDO::FETCH_ASSOC);
+  foreach ($checks as $check) {
+
+  print_r($check);
+  }
+}
+catch(\Exception $e){
+    print_r($e);
+}
+echo '</pre>';
+
+?>
