ba9a67846e29d544c7141825e6a252cda196eda3
psable <52033359+psable@users.noreply.github.com>
2021-03-15 20:23:15 +0100

    Create frontend.php

---
+++ frontend.php
@@ -0,0 +1,21 @@
+<?php
+  $color= getenv('COLOR') ? getenv('COLOR') : "blue";
+  $backend_name = getenv('BACKEND') ? getenv('BACKEND') : "backend";
+  $backend_url="http://".$backend_name."/index.php";
+
+  function get_hostname() {
+    global $color;
+    return "<font color=\"" . $color . "\">" . gethostname() . "</font>";
+  }
+
+  function call_backend() {
+    global $backend_url;
+    $worker = file_get_contents($backend_url);
+    return $worker;
+  }
+?>
+<html><head><title>Swarm Service Discovery and Load Balancing Demo</title></head>
+<body>
+<h2>Traitement frontend effectu&eacute; par : <?php echo get_hostname(); ?></h2>
+<h2>Traitement backend effectu&eacute; par : <?php echo call_backend(); ?></h2>
+</body>
