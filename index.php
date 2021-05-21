bd271b37c543d0e5a5f736a8b9ecbe99811871e5
psable <52033359+psable@users.noreply.github.com>
2021-05-21 11:19:57 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 echo "<h1>Hello World from PHP7-FPM</h1>";
 echo 'Version PHP courante : ' . phpversion();
-echo 'Mon nom d\'utilisateur est ' .$_ENV["USER"] . '!';
+echo 'Mon hostname est ' .$_ENV["HOSTNAME"] . '!';
 
 
 echo '<pre>';
