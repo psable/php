0bb7cb66f5812258e8918183a374601c2d052640
psable <52033359+psable@users.noreply.github.com>
2021-05-21 12:58:31 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,10 @@
 <?php
 echo "<h1>Hello World from PHP7-FPM</h1>";
 echo 'Version PHP courante : ' . phpversion();
-$hostname_perso = getenv ( string $varname , bool $local_only = true) : string|false;
-echo 'Mon hostname est ' .$hostname_perso '!';
-
 
 echo '<pre>';
 try{
-  $conn = new \PDO('mysql:host=mariadb-service', 'root', 'roottoor');
+  $conn = new \PDO('mysql:host=mariadb', 'root', 'roottoor');
   $sth = $conn->prepare('SHOW DATABASES');
   $sth->execute();
   $checks = $sth->fetchAll(PDO::FETCH_ASSOC);
