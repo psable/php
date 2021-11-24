5aaf39db5179358de2cc4c8c675e01aca71475c8
psable <52033359+psable@users.noreply.github.com>
2021-11-24 13:25:13 +0100

    Update myphp.php

--- myphp.php
+++ myphp.php
@@ -2,7 +2,4 @@
 <?php
 echo "<h1>Hello World from PHP7-FPM</h1>";
 echo 'Version PHP courante : ' . phpversion();
-
-getenv(string $BACKEND_PORT_80_TCP, bool $local_only = false): string|false BACKEND_PORT_80_TCP
-
 ?>
